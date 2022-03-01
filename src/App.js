6ee68da5fea7d036d1dac40def74c6bc430a9e62
Jeffrey Rose <jtr219@outlook.com>
2022-03-01 15:37:17 -0500

    test react auth

--- src/App.js
+++ src/App.js
@@ -6,6 +6,22 @@ import axios from "axios";
 function App() {
   const [state, setState] = useState("");
 
+  const testLogin = () => {
+    axios
+      .get("https://gdd-server.herokuapp.com/login")
+      .then((response) => console.log(response));
+  };
+  const testLogout = () => {
+    axios
+      .get("https://gdd-server.herokuapp.com/logout")
+      .then((response) => console.log(response));
+  };
+  const getProtected = () => {
+    axios
+      .get("https://gdd-server.herokuapp.com/protected")
+      .then((response) => console.log(response));
+  };
+
   useEffect(() => {
     axios.get("https://gdd-server.herokuapp.com").then((response) => {
       setState(response.data);
@@ -15,6 +31,9 @@ function App() {
     <div className="App">
       <h1>GDD</h1>
       <p>{String(state)}</p>
+      <button onClick={testLogin}>Login</button>
+      <button onClick={testLogout}>Logout</button>
+      <button onClick={getProtected}>protected?</button>
     </div>
   );
 }
