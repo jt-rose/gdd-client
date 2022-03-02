8732cceb5520cd4d4c2a04168327d3a8da81c228
Roderick Houston <rsjm318@yahoo.com>
2022-03-02 15:14:57 -0500

    user update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,48 @@
 import { Link } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+import { ChakraProvider, Box, Button,  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton, useDisclosure, Container} from '@chakra-ui/react'
+
 
 export const Home = () => {
+
+const { isOpen, onOpen, onClose } = useDisclosure()
   return (
     <>
-      <h1>Home Page</h1>;<Link to="/design">Design Doc</Link>
-      <Link to="/search">Search projects</Link>
+
+      <Box className="main">
+          <Box className="mainEffect">
+              <Box  className="mainContainer" >
+                <Box className='header' padding='4' color='white'>
+                <Navbar />
+                <h1>Home Page</h1>
+                <Link className='links' to="/design">Design Doc</Link>
+                <Link className='links' to="/search">Search projects</Link>
+
+                </Box>
+                <Box className= "content">
+                    <Box className= "contentLeft">
+
+                    </Box>
+                    <Box className= "contentRight">
+                        <Container maxW='xl' centerContent>
+                            <Box padding='4' bg='gray.100' maxW='3xl'>
+                                There are many benefits to a joint design and development system. Not only
+                                does it bring benefits to the design team.
+                            </Box>
+                        </Container>
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    </Box>
+
+
     </>
   );
 };
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,60 @@
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+} from '@chakra-ui/react'
+import {useState} from 'react';
+import axios from 'axios'
+import {post} from '../utils/serverURL.js'
+
+
 export const Register = () => {
+
+    const [user, setNewUser] = useState({image:'./pfPic.jpeg'});
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setNewUser({ ...user, [name]: value });
+    };
+
+    const handleNewUser = (e) => {
+        e.preventDefault();
+        axios.post(
+            '/user/register',
+            {
+                name: user.name,
+                password: user.password,
+                email: user.email,
+                company: user.company,
+                description: user.description,
+                location: user.location,
+                image: user.image,
+            }
+        )}
+
+
   return (
     <>
       <h1>Register</h1>;
+      <form onSubmit={handleNewUser}>
+          <div id="formBox">
+              <div className='pairs'>
+                  UserName: <input className="input"  name='username' onChange={handleChange}/><br/>
+                  Password: <input className="input"  name='password' onChange={handleChange}/><br/>
+              </div>
+              <div className='pairs'>
+                  Email: <input className="input" name="email" id="species" onChange={handleChange}/><br/>
+                  Company: <input className="input" name='company' onChange={handleChange}/><br/>
+              </div>
+              <div className='pairs'>
+                  Location <input className="input" name='location' onChange={handleChange}/><br/>
+                  Description <input className="input" name='description' onChange={handleChange}/><br/>
+                  Image url: <input className="input" name='image'  onChange={handleChange}/><br/>
+              <input id='buttForm1' className='butt' type="submit" value="Submit"/>
+              </div>
+          </div>
+      </form>
     </>
   );
 };
